42f0f67fa53a7fcf80e2279ec4e9ba85b3bf67bb
mgxian <will835559313@163.com>
2021-02-17 13:25:49 +0800

    fix: make sinon promise test work

--- src/views/__tests__/ItemList.spec.js
+++ src/views/__tests__/ItemList.spec.js
@@ -1,18 +1,21 @@
 import sinon from 'sinon'
 import { expect } from 'chai'
 import { shallowMount } from '@vue/test-utils'
+import flushPromises from 'flush-promises'
 import Item from '@/components/Item.vue'
 import ItemList from '../ItemList.vue'
-// import { fetchListData } from '@/api/api'
+import * as api from '@/api/api'
 
 describe('ItemList.vue', () => {
-  it('renders an Item with data for each item in window.items', () => {
+  it('renders an Item with data for each item in window.items', async () => {
     const items = [{ id: 1 }, { id: 2 }, { id: 3 }]
     const $bar = {
       start: () => {},
       finish: () => {}
     }
+    sinon.stub(api, 'fetchListData').resolves(items)
     const wrapper = shallowMount(ItemList, { mocks: { $bar } })
+    await flushPromises()
     const Items = wrapper.findAllComponents(Item)
     expect(Items.length).to.equal(items.length)
     Items.wrappers.forEach((wrapper, i) => {
